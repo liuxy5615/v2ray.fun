c5dbe2f3fc33ff33370aad7b2e0a86bd50e03f7a
liuxy5615 <46519310+liuxy5615@users.noreply.github.com>
2019-10-17 11:28:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,27 +36,27 @@ curl  https://get.acme.sh | sh
 
 #克隆V2ray.fun项目
 cd /usr/local/
-rm -R v2ray_func
-git clone https://github.com/liuxy5615/v2ray_func.git
-cd v2ray_func && git checkout origin/kiriMod
+rm -R v2ray.fun
+git clone https://github.com/liuxy5615/v2ray.fun.git
+cd v2ray.fun && git checkout origin/kiriMod
 
 #安装V2ray主程序
 bash <(curl -L -s https://install.direct/go.sh)
 
 #配置V2ray初始环境
-cp /usr/local/v2ray_func/v2ray /usr/local/bin
+cp /usr/local/v2ray.fun/v2ray /usr/local/bin
 chmod +x /usr/bin/v2ray
 chmod +x /usr/local/bin/v2ray
 rm -rf /etc/v2ray/config.json
-mv /usr/local/v2ray_func/json_template/server.json /etc/v2ray/config.json
+mv /usr/local/v2ray.fun/json_template/server.json /etc/v2ray/config.json
 UUID=$(cat /proc/sys/kernel/random/uuid)
 sed -i "s/cc4f8d5b-967b-4557-a4b6-bde92965bc27/${UUID}/g" /etc/v2ray/config.json
-python /usr/local/v2ray_func/genclient.py
-python /usr/local/v2ray_func/openport.py
+python /usr/local/v2ray.fun/genclient.py
+python /usr/local/v2ray.fun/openport.py
 service v2ray restart
 
 
 clear
 
-echo "V2ray_func 安装成功！By: Masteen"
+echo "V2ray.fun 安装成功！By: Masteen"
 echo "输入 v2ray 回车即可使用"
